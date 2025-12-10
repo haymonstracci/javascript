873aeb24bc1a4eeee667901f3a6e4efb55b05764
Haymon Stracci <straccicesar@gmail.com>
2025-12-10 00:01:05 -0300

    repetições parte 1

---
+++ aula013/node_ae334d1d76a3e.tmp.js
@@ -0,0 +1,16 @@
+var c = 1
+while (c <= 500) {
+    console.log('Tudo bem?')
+    
+}
+
+
+
+/* 
+  console.log('Tudo bem?')
+  console.log('Tudo bem?')
+  console.log('Tudo bem?')
+  console.log('Tudo bem?')
+  console.log('Tudo bem?')
+  console.log('Tudo bem?')
+*/
