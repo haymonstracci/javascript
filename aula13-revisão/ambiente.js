8f0ff71b0f2a1a88f9988a47d2eb8b931f6cb0ec
Haymon Stracci <straccicesar@gmail.com>
2026-03-01 06:02:37 -0300

    revisão ex13

--- aula13-revisão/ambiente.js
+++ aula13-revisão/ambiente.js
@@ -11,14 +11,33 @@ while (c <= 6) {
 }
  */
 
-
-
-
 /*console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')*/
+
+
+// exercícios práticos. (1)
+
+for (let i = 0; i <= 10; i++) {
+
+  if (i % 2 === 0) {
+    console.log(`${i} é par`)
+  } else {
+    console.log(`${i} é impar`)
+  }
+
+}
+
+// exercícios práticos. (2)
+
+for (let i = 1; i <= 20; i++) {
+    if (i > 5 && i < 15 && i % 2 === 0) {
+        console.log(i)
+    }
+        
+}
 
 
 
