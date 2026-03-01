aeae3192fe806577c3cb213300371adcac6214b6
Haymon Stracci <straccicesar@gmail.com>
2026-03-01 19:31:25 -0300

    exercícios de fixação.

--- aula13-revisão/ambiente01.js
+++ aula13-revisão/ambiente01.js
@@ -1,8 +1,8 @@
-var c = 1 
+var c = 1;
 do {
-    console.log(`Passo ${c}`)
-    c++ // c = c + 1
-} while (c <= 6)
+  console.log(`Passo ${c}`);
+  c++; // c = c + 1
+} while (c <= 6);
 
 /* var c = 1 
 while (c <= 6) {
@@ -17,28 +17,20 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')*/
 
-
 // exercícios práticos. (1)
 
 for (let i = 0; i <= 10; i++) {
-
   if (i % 2 === 0) {
-    console.log(`${i} é par`)
+    console.log(`${i} é par`);
   } else {
-    console.log(`${i} é impar`)
+    console.log(`${i} é impar`);
   }
-
 }
 
 // exercícios práticos. (2)
 
 for (let i = 1; i <= 20; i++) {
-    if (i > 5 && i < 15 && i % 2 === 0) {
-        console.log(i)
-    }
-        
+  if (i > 5 && i < 15 && i % 2 === 0) {
+    console.log(i);
+  }
 }
-
-
-
-
---
+++ aula13-revisão/ambiente02.js
@@ -0,0 +1,9 @@
+var país = "Brasil";
+console.log("vivendo em ${país}");
+if ((país = "Brasil")) {
+  console.log("Você é Brasileiro!");
+} else {
+  console.log("Você é estrangeiro!");
+}
+
+
